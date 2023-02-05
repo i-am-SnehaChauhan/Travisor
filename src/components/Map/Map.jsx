da9b9fa203db15da51dfa9738d1d38b021d02549
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-02-05 15:24:27 +0530

    map Style

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import googleMapReact from 'google-map-react';
+import { Paper, Typography, usrMediaQuery } from '@material-ui/core';
+import LocationOutLinedIcon from '@material-ui/icons/LocationOutLined';
+import  Rating  from '@material-ui/lab';
 
+import useStyles from './style';
 const Map = () => {
+  const classes = useStyles
   return (
     <h1>Map</h1>
   )
